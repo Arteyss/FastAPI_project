8d58b911698d58c0434df2148ca9709d9e9b6453
Arteyss <olejnickovturistdgofs@gmail.com>
2023-05-19 18:25:32 +0300

    service mini fix

--- App/service.py
+++ App/service.py
@@ -22,7 +22,7 @@ def convert_wav_mp3(file):
     if file.content_type != "audio/wav":
         raise HTTPException(status_code=400, detail="Only wav files are allowed")
     file_id = generate_token()
-    mp3_path = f"./audio/{file_id}.mp3"
+    mp3_path = f"audio/{file_id}.mp3"
 
     sound = AudioSegment.from_wav(file.file)
     sound.export(mp3_path, format="mp3")
