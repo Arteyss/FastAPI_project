c134dffa8d0e04b9e875b548372aae4ba1c43634
Arteyss <olejnickovturistdgofs@gmail.com>
2023-05-19 12:17:50 +0300

    models.py | add User model

--- App/models/models.py
+++ App/models/models.py
@@ -12,3 +12,12 @@ class Quiz(Base):
     question = Column(String, index=True)
     answer = Column(String, index=True)
     created_at = Column(DateTime, index=True)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True,
+                autoincrement=True)
+    name = Column(String(64), unique=True, index=True)
+    token = Column(String)
